b00f46012e952c84f4795dda8c6d2291806a7edc
milankostak <milankostak>
2016-07-15 11:07:13 +0800

    brazil screen

---
+++ app/src/main/java/cz/uhk/fivethings/BrazilScreen.java
@@ -0,0 +1,19 @@
+package cz.uhk.fivethings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BrazilScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.brazil_layout);
+        initComponents();
+    }
+
+    private void initComponents() {
+
+    }
+
+}
